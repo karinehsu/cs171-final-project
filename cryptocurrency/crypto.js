e26139bb5ffae53b30e018a86503b5c85228ca69
Alex Liu <zezhouliu@college.harvard.edu>
2014-04-03 16:09:00 -0400

    Cleaned up code and added ajax skeleton

--- cryptocurrency/crypto.js
+++ cryptocurrency/crypto.js
@@ -8,3 +8,52 @@ var margin = {
     left: 50
 };
 
+var width = 1280 - margin.left - margin.right;
+var height = 760 - margin.bottom - margin.top;
+
+var bbVis = {
+    x: 100,
+    y: 10,
+    w: width - 2 * 100,
+    h: height - 2 * 10
+};
+
+// main canvas for our visualization
+var main_canvas = d3.select("#vis").append("svg").attr({
+    width: width + margin.left + margin.right,
+    height: height + margin.top + margin.bottom
+})
+
+// append a svg
+var svg = canvas.append("g").attr({
+        transform: "translate(" + 0 + "," + 0 + ")"
+    })
+    .append("g")
+    .attr({
+        transform: "translate(" + margin.left + "," + margin.top + ")"
+});
+
+var runAjaxQuery = function () {
+
+    $.ajax({
+        url: "https://www.cryptocoincharts.info/v2/tools/api",
+        async: true,
+        jsonpCallback: 'getdata',
+        dataType: 'jsonp',
+        success: function (data, status) {
+
+            // check status, then update accordingly
+            if (status == "success") {
+
+            }
+            else {
+                // if fails, then debug
+            }
+        }
+
+    });
+
+}
+
+
+
